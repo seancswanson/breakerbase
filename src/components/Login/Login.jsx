2e5b19c62c8248509b65d587c9b7b45412936a77
Sean Swanson <sean-swan@hotmail.com>
2020-05-17 21:28:31 -0700

    Added auth form inside Login page component.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Card, Form, Input, Button, Checkbox } from 'antd';
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -10,17 +26,78 @@ class Login extends Component {
   render() {
     const { mockLogin } = this.props;
 
+    const onFinish = (values) => {
+      console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+    };
+
     return (
-      <div>
-        Login!!{' '}
-        <Button
-          type="primary"
-          onClick={() => {
-            mockLogin('in');
-          }}
-        >
-          Log In
-        </Button>
+      <div
+        style={{
+          display: 'flex',
+          flex: '1',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Card title="Log In" style={{ width: '50vw' }}>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          <Button
+            type="primary"
+            onClick={() => {
+              mockLogin('in');
+            }}
+          >
+            Log In
+          </Button>
+        </Card>
       </div>
     );
   }
