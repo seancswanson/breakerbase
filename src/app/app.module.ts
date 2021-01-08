a02452f762e1f3ca0d51df433eda8abba0b86e35
seanswanson <sean.swanson@dreambox.com>
2021-01-08 09:36:16 -0800

    Added Antd framework.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,16 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { SessionLogComponent } from './session-log/session-log.component';
 import { TrainerComponent } from './trainer/trainer.component';
 import { SkillLibraryComponent } from './skill-library/skill-library.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -26,9 +36,14 @@ import { SkillLibraryComponent } from './skill-library/skill-library.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzTableModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
