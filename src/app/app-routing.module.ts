ea9b8bc86c88932403edf549be6a3ae702cbdf03
seanswanson <sean.swanson@dreambox.com>
2021-01-08 09:18:11 -0800

    Added login route. Added user profile link to header. Added helper class for feature wrappers.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JoinPageComponent } from './join-page/join-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { SessionLogComponent } from './session-log/session-log.component';
 import { SkillLibraryComponent } from './skill-library/skill-library.component';
 import { TrainerComponent } from './trainer/trainer.component';
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
   path: "join",
   component: JoinPageComponent
+},
+  {
+  path: "login",
+  component: LoginPageComponent
 },
   {
   path: "session-log",
