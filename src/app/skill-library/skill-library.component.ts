f4bd548c1cec308486d28e8cbe3385ad9f03f04a
seanswanson <sean.swanson@dreambox.com>
2021-01-13 09:33:13 -0800

    Created Video interface to construct array of Video objects to display. Now retrieving some videos based on a query and logging it.

--- src/app/skill-library/skill-library.component.ts
+++ src/app/skill-library/skill-library.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from './youtube.service';
+import { Video } from './video.interface';
 
 @Component({
   selector: 'app-skill-library',
   templateUrl: './skill-library.component.html',
   styleUrls: ['./skill-library.component.scss']
 })
-export class SkillLibraryComponent implements OnInit {
 
-  constructor() { }
+export class SkillLibraryComponent implements OnInit {
+  videos: Video[] = [];
+
+  constructor(private youtube: YoutubeService) {
+    youtube.getVideos('bboy').subscribe((response) => {
+      console.log(response);
+    })
+  }
 
   ngOnInit() {
   }
