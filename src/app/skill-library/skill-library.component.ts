70531028ee93280cb1eccc906c4d8abd3f40e0aa
seanswanson <sean.swanson@dreambox.com>
2021-01-19 09:31:11 -0800

    Now rendering iframes and displaying titles for bboy history videos.

--- src/app/skill-library/skill-library.component.ts
+++ src/app/skill-library/skill-library.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from './youtube.service';
 import { Video } from './video.interface';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-skill-library',
@@ -11,9 +12,22 @@ import { Video } from './video.interface';
 export class SkillLibraryComponent implements OnInit {
   videos: Video[] = [];
 
-  constructor(private youtube: YoutubeService) {
-    youtube.getVideos('bboy').subscribe((response) => {
+  constructor(private youtube: YoutubeService, private sanitizer: DomSanitizer) {
+    youtube.getVideos('bboy history').subscribe((response) => {
       console.log(response);
+      this.videos = response.map(item => {
+        return {
+          title: item.snippet.title,
+          videoId: item.id.videoId,
+          videoUrl: sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${item.id.videoId}`),
+          channelId: item.snippet.channelId,
+          channelUrl: `https://www.youtube.com/channel/${item.snippet.channelId}`,
+          channelTitle: item.snippet.channelTitle,
+          description: item.snippet.description,
+          publishedAt: new Date(item.snippet.publishedAt),
+          thumbnail: item.snippet.thumbnails.high.url
+        };
+      });
     })
   }
 
